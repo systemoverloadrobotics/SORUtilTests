8188115d894f4211a62097fdbb863e3d8bc42e58
Jeff Spragg <jwspragg@mtu.edu>
2023-03-19 03:14:00 -0700

    Add tests for Circle snapping

--- SorMathTest.java
+++ SorMathTest.java
@@ -1,5 +1,6 @@
 package frc.sorutil;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -8,7 +9,21 @@ import org.junit.jupiter.api.Test;
 public class SorMathTest {
   @Test
   public void polarTest() {
+    double[] polarified = SorMath.cartesianToPolar(2, 8);
 
+    double expectedR = 2 * Math.sqrt(17);
+    double expectedTheta = Math.toDegrees(Math.atan(4));
+
+    assertTrue(SorMath.epsilonEquals(expectedR, polarified[0]), "Magnitude result from cartesianToPolar is not equal");
+    assertTrue(SorMath.epsilonEquals(expectedTheta, polarified[1]), "Angle result from cartesianToPolar is not equal");
+
+    polarified = SorMath.cartesianToPolar(-2, -4);
+
+    expectedR = 2 * Math.sqrt(5);
+    expectedTheta = Math.toDegrees(Math.atan(2) + Math.PI);
+
+    assertTrue(SorMath.epsilonEquals(expectedR, polarified[0]), "Magnitude result from cartesianToPolar is not equal");
+    assertTrue(SorMath.epsilonEquals(expectedTheta - 360, polarified[1]), "Angle result from cartesianToPolar is not equal");
   }
 
   @Test
@@ -35,5 +50,14 @@ public class SorMathTest {
     assertTrue(SorMath.epsilonEquals(result, 100),
         "Result from interpolate is approximately equal to expected when low == high");
 
+  }
+
+  @Test 
+  public void circleSnappingTest() {
+    int snapped = SorMath.circleSnappingDegrees(36, 8);
+    assertEquals(45, snapped, "Expected a circle with 8 divisions (45 degrees) to snap 36 degrees to 45");
+
+    snapped = SorMath.circleSnappingDegrees(-38, 10);
+    assertEquals(324, snapped, "Expected a circle with 10 divisions (36 degrees) to snap -38 degrees to 324");
   }
 }
